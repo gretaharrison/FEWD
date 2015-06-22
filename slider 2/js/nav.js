569db7685b07ecc2fad454e2837625e22bf2e3d2
gretaharrison <gretarrison@gmail.com>
2015-06-22 16:51:32 -0700

    homework update

---
+++ slider 2/js/nav.js
@@ -0,0 +1,19 @@
+Element.prototype.nav = function() {
+// put logic for opening and closing nav in here.
+  var nav = this,
+  wrapper = nav.children[0],
+  position = 0;
+  width = window.innerWidth,
+
+  navBtn = document.createElement('div');
+
+  this.createButtons = function(){
+
+    navBtn.classList.add('nav');
+
+    navBtn.addEventListener('mousedown',function(){
+    
+      }
+    });
+
+}
