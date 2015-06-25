87c78923aed1dc9f52e0a6484c90252b0cdf4634
gretaharrison <gretarrison@gmail.com>
2015-06-24 20:17:07 -0700

    In class work

--- gallery/js/gallery.js
+++ gallery/js/gallery.js
@@ -1,60 +1,44 @@
-//Define prototypical Gallery function
 Element.prototype.Gallery = function(){
   var gallery = this;
   var ul = gallery.children[0];
   var photos = {};
-  // {}; means "empty object"
+  var singlePhotoSection = document.getElementById('single-photo');
+  var container = document.getElementById('container');
+
   // Define global variables
+  this.singlePhoto = function (ev){
+    var section = document.createElement('section');
 
-  this.singlePhoto = function(ev){
-    console.log(ev);
+    section.classList.add('single-photo');
+    section.innerHTML = ev.target.innerHTML;
+    section.style.backgroundImage = ev.target.style.backgroundImage;
+
+    var closeButton = document.createElement('div');
+    closeButton.classList.add('close');
+    closeButton.addEventListener('click',function() {
+      section.style.display = 'none';
+    });
+    section.appendChild(closeButton);
+    container.appendChild(section);
   };
-
 
   this.layoutPhotos = function(){
-      // add logic for each photo in here
-
-      photos.forEach(function(photo, index){
-
-        console.log(photo);
-        var li = document.createElement('li');
-
-        li.style.backgroundImage = 'url("'+photo.image_url+'")';
-        li.style.backgroundSize = 'cover';
-
-        li.innerHTML = '<div class="meta"><h5>'+
-        photo.name+
-        '</h5><h6>'+
-        photo.user.fullname+
-        '</h6></div><div class="stats"><div>'+
-        photo.rating+'</div></div>'+
-        '</div>';
-
-        li.addEventListener('click', function()
-        {
-
-          li.innerHTML = '<div class="single-photo"></div>';
-          li.style.single-photo;
-          li.style.width = '100%';
-          li.style.backgroundSize = 'cover';
-
-
-
-        });
-
-        ul.appendChild(li);
-
-      });
-
+    photos.forEach(function(photo,index){
+      var li = document.createElement('li');
+      li.style.backgroundImage = 'url("'+photo.image_url+'")';
+      li.style.backgroundSize = 'cover';
+      li.innerHTML = '<div class="meta"><h5>'+
+      photo.name+
+      '</h5><h6>'+
+      photo.user.fullname+
+      '</h6></div><div class="stats"><div>'+
+      photo.rating+'</div></div>'+
+      '</div>';
+      // add single photo page here
+      li.addEventListener('mousedown',gallery.singlePhoto);
+      ul.appendChild(li);
+    });
   };
-
-  this.singlePhoto = function(){
-    // link to single photo css using innerHTML
-
-  };
-
-
-
 
   this.connect = function(){
       var xhr = new XMLHttpRequest();
